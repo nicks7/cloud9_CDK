a6077cf70bbd9d3ff54fa6e5bb453c9a1c307006
Arushi Singh <2000077260@hexaware.com>
2023-03-30 18:06:40 +0530

    files changes

--- lib/sustainability-cdkapp-stack.ts
+++ lib/sustainability-cdkapp-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import { Construct } from 'constructs';
 import { Period } from "aws-cdk-lib/aws-apigateway";
 import { Code, Function, Runtime, Architecture } from 'aws-cdk-lib/aws-lambda';
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class SustainabilityCdkappStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -94,17 +97,106 @@ export class SustainabilityCdkappStack extends cdk.Stack {
     //======================== API -> Lambda ends
 
     /*
-    S3 Bucket
+    S3 Bucket: to hold static contents from Salesforce
     */
     const siteBucket = new s3.Bucket(this, "Bucket", {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       publicReadAccess: false,
       versioned: false,
-      bucketName: `sustainability-proof-of-concept`,
+      bucketName: `cloud9-sustainability-poc-ui-bucket`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       encryption: s3.BucketEncryption.S3_MANAGED,
       autoDeleteObjects: true,
       enforceSSL: true,
     });
+
+    /*
+    S3 Bucket: Common logging bucket
+    */
+    const logBucket = new s3.Bucket(this, "LogBucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      versioned: false,
+      bucketName: `common-logging-cloud9-sustainability-poc-ui-bucket`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      autoDeleteObjects: true,
+      enforceSSL: true,
+    });
+
+    /*
+    OAI: CloudFront OAI configuration
+    */
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      "sfimage-cloudfront-OAI",
+      {
+        comment: "OAI for Salesforce static image CDN",
+      }
+    );
+
+    /*
+    BucketPolicy: Providing CloudFront access to S3 contents
+    */
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    /*
+    CloudFront: Distribution configuration
+    */
+    const distribution = new cloudfront.Distribution(
+      this,
+      `cloud9-sustainability-siteDistribution`,
+      {
+        defaultRootObject: "index.html",
+        errorResponses: [
+          {
+              httpStatus: 404,
+              responseHttpStatus: 200,
+              responsePagePath: '/index.html',
+          },
+        ],
+        comment: "This distribution hosts the static images from Salesforce",
+        minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+        enableLogging: true,
+        logFilePrefix: `cloud9-sustainability-cloudfront`,
+        logBucket: logBucket,
+        defaultBehavior: {
+          origin: new cloudfront_origins.S3Origin(siteBucket, {
+            originAccessIdentity: cloudfrontOAI,
+          }),
+          compress: true,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }
+    );
+
+    /*
+    S3 Upload: Deploying Salesforce static images to S3 Bucket
+    */
+    new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
+      sources: [s3deploy.Source.asset("./site-contents")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.domainName,
+      description: 'The distribution URL',
+      exportName: 'CloudfrontURL',
+  });
   }
 }
